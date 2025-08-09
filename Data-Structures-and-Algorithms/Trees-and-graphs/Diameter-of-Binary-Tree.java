bff690ab17b10ed73e4093287614170fd1d44dcc
Valdemiro Vieira <valdemirovieira@MacBook-Air-de-Valdemiro.local>
2025-08-09 20:34:35 -0300

    Adding solution to course Data Structures and Algorithms, section Trees and graphs, question Diameter of Binary Tree, coded in Java language

---
+++ Data-Structures-and-Algorithms/Trees-and-graphs/Diameter-of-Binary-Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter = 0;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return diameter;
+    }
+    
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        diameter = Math.max(diameter, left+right+2);
+        return Math.max(left, right) + 1;
+    }
+}
